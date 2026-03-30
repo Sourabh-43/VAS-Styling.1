de157c4f5a043755e84931bf8f363252e183d2b6
Sourabh Yadav <sourabh4349@gmail.com>
2026-03-30 16:22:33 +0530

    Fix admin oders

--- frontend/src/app/features/admin/admin-orders/admin-orders.ts
+++ frontend/src/app/features/admin/admin-orders/admin-orders.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrderService } from '../../../core/services/order.service';
 import { Signal } from '@angular/core';
@@ -11,9 +11,10 @@ import { Order } from '../../../core/models/order';
   templateUrl: './admin-orders.html',
   styleUrls: ['./admin-orders.scss']
 })
-export class AdminOrdersComponent  implements OnInit {
+export class AdminOrdersComponent implements OnInit {
 
   orders!: Signal<Order[]>;
+  expandedOrder = signal<string | null>(null);
 
   constructor(private orderService: OrderService) {}
 
@@ -21,7 +22,14 @@ export class AdminOrdersComponent  implements OnInit {
     this.orders = this.orderService.orders;
   }
 
-  changeStatus(orderId: string, status: 'pending' | 'paid' | 'shipped') {
+  changeStatus(orderId: string, status: Order['status']) {
     this.orderService.updateStatus(orderId, status);
   }
+
+  toggle(orderId: string) {
+    this.expandedOrder.set(
+      this.expandedOrder() === orderId ? null : orderId
+    );
+  }
+
 }
