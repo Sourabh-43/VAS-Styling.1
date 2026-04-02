278191d9c023b6fdbc99868d344cacd7af32027e
Sourabh Yadav <sourabh4349@gmail.com>
2026-04-02 17:36:53 +0530

    fix routes issues

--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const controller = require('../controllers/product.controller');
+const { protect, admin } = require('../middleware/auth');
 
 /* GET ALL PRODUCTS */
 router.get('/', controller.getProducts);
@@ -9,5 +10,31 @@ router.get('/search', controller.searchProducts);
 
 /* GET PRODUCT BY ID */
 router.get('/:id', controller.getProductById);
+
+/* CREATE PRODUCT */
+router.post(
+  '/',
+  protect,
+  admin,
+  controller.upload,
+  controller.createProduct
+);
+
+/* UPDATE PRODUCT */
+router.put(
+  '/:id',
+  protect,
+  admin,
+  controller.upload,
+  controller.updateProduct
+);
+
+/* DELETE PRODUCT */
+router.delete(
+  '/:id',
+  protect,
+  admin,
+  controller.deleteProduct
+);
 
 module.exports = router;
